4d06755979fec00f61ba1081e93a17689fb43aa2
diegovg0104 <38222518+diegovg0104@users.noreply.github.com>
2018-05-23 13:21:43 -0500

    1subida

---
+++ js/acciones3.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
+		$('#izquierda').on("swipeleft", function(){
+			navigator.notification.alert("deslizo hacia la izquierda", function(){"aplicacion 7","Ok"});
+			});//cerrar deslizar izquierda 
+		$('#derecha').on("swiperight", function(){
+			navigator.notification.confirm("¿Que quieres hacer?", function(op){
+				switch(op)
+				{
+					case 1:
+					 navigator.notification.beep(1);
+					break;
+					case 2:
+					 navigator.notification.vibrate(3000);
+					break;
+					
+					}
+				},"aplicacion 7","Beepear,Vibra,Cancelar");
+			}); //cerrar deslizar derecha
+		},false);//cerrar deviceready
+});// cerrar document
+
